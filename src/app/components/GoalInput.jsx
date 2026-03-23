6d2520746703a8c5ea7457176df7331c8920a5e5
Don Joseph <djoseph@opentext.com>
2026-03-23 17:30:34 -0400

    create components file

---
+++ src/app/components/GoalInput.jsx
@@ -0,0 +1,13 @@
+
+
+function GoalInput(){
+    return(
+        <>
+        <div>
+            <h1>What would you like to achieve by the end of 2026?</h1>
+        </div>
+        </>
+    );
+}
+
+export default GoalInput;
